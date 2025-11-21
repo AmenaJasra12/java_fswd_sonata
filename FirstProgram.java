1c448a2c64f7c51129d01a38b333124a66029b18
Sadia Amena <naazhania917@gmail.com>
2025-11-21 19:22:01 +0530

    wrote main fuction

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("My name is "+args[0]);
+        System.out.println("My age is "+args[1]+" years old");
+    }
+}
