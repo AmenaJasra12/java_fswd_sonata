3f32235a00bacb2067c725d34cb52795f642b3e1
Sadia Amena <naazhania917@gmail.com>
2025-11-24 16:58:29 +0530

    hands on

---
+++ lords_college_java_training/src/main/java/org/example/data_type/NonPrimitiveDataType.java
@@ -0,0 +1,9 @@
+package org.example.data_type;
+public class NonPrimitiveDataType{
+    public static void main(String[] args){
+        String name ="Amena";
+        System.out.printf("Name :"+name);
+        int[] array1={1,2,4,5};
+
+    }
+}
